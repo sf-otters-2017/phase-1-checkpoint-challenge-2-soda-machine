28430f470c09b6e4e61a925f96247c50abb771ec
Samantha Gomez <sgomez_21@yahoo.com>
2016-12-13 10:06:15 -0800

    final checkpoint

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,29 @@
 class SodaMachine
   attr_reader :sodas, :cash
 
-  def initialize(args = {})
-    @sodas = args[:sodas]
-    @cash = args[:cash]
+  def initialize(info = {})
+    @sodas = info[:sodas]
+    @cash = info[:cash]
   end
 
   def current_inventory_count
+     @sodas
   end
 
   def find_soda(soda_brand)
+    if @sodas == soda_brand
+      return soda_brand
+    else @sodas != soda_brand
+      return nil
+    end
   end
 
   def sell(soda_brand)
+  # if @soda != soda_brand
+  #     return nil
+  #   else
+
+  #   end
   end
 
 end
