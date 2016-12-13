f918aa71347200ac78275a4374712b2aa5297fad
Sean Wright <email": null>
2016-12-13 10:03:34 -0800

    Edit with checkpoint challenge changes.

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    soda_check = @sodas.each do |soda|
+      if soda.brand == soda_brand
+        return soda
+      end
+    end
+
   end
 
   def sell(soda_brand)
+    if @sodas.each do |soda|
+      if soda.brand == soda_brand
+        @cash += soda.price
+        return soda
+      end
+    end
   end
+end
 
 end
