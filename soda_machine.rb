6555d511d33a2f9d0967bf6fd42957293d06bc05
Kornelius <ip2665@gmail.com>
2016-12-13 10:03:02 -0800

    Complete the soda_machine behaviour to pass the test

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.size
   end
 
   def find_soda(soda_brand)
+    sodas.each do |soda|
+      return soda if soda.brand == soda_brand
+    end
+    return nil
   end
 
   def sell(soda_brand)
+      sodas.each_with_index do |soda,idx|
+       if soda.brand == soda_brand
+
+         @cash += soda.price
+          sodas.delete_at(idx)
+         return soda
+       end
+    end
+    return nil
   end
 
 end
