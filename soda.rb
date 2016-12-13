1e9bd03fb4e631d091bf496ce2f100051698342c
Jack Dagley <jack.dagley77@gmail.com>
2016-12-13 10:01:49 -0800

    add method for machine to find a brand name and delete if it sells

--- soda.rb
+++ soda.rb
@@ -6,3 +6,11 @@ class Soda
     @price = args[:price]
   end
 end
+
+# Soda.new = {
+# brand: 'Pepsi', price: 0.65,
+# brand: 'Mountain Dew', price: 0.75,
+# brand: 'Coke Zero', price: 1.00,
+# brand: 'Pepsi', price: 0.65
+# }
+# p soda.args
--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,32 @@
 class SodaMachine
   attr_reader :sodas, :cash
 
+#args = {sodas: [brand, price],
+#        cash: price
+#         }
+
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
+#access the soda from the array in which brand = soda_branch
+#@sodas includes instance variables of each soda stored in an array
   def find_soda(soda_brand)
+    @sodas.find {|soda_name| soda_name.brand == soda_brand}
   end
 
+#remove from sodas array, and subract price from cash
   def sell(soda_brand)
+    @sodas.delete_if {|soda| soda.brand == soda_brand}
   end
-
 end
+#new sodas are created. they each have a brand and a price
+#the sodas are stored in an array and passed as args to the soda machine
+
+machine = SodaMachine.new
+p machine.sodas
