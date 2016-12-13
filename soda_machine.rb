bcb5c596705681c1c4f1a6aefeff348016914eb9
Jay Naidu <jnaidu@mail.sfsu.edu>
2016-12-13 09:37:21 -0800

    Passes tests for soda_machine_spec.rb

--- soda_machine.rb
+++ soda_machine.rb
@@ -4,15 +4,23 @@ class SodaMachine
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    @sold_soda = ""
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.find { |soda| soda.brand == soda_brand }
   end
 
   def sell(soda_brand)
+    @sold_soda = find_soda(soda_brand)
+    unless @sold_soda == nil
+      @cash += @sold_soda.price
+      @sodas.delete(@sold_soda)
+    end
   end
 
 end
