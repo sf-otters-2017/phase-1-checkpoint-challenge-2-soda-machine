d40abc836c68f1b76765b3388efd8ff04512b4c5
rdfm <raulmenchavez@gmail.com>
2016-12-13 09:37:22 -0800

    Adds pseudocode to soda_machine.rb

--- soda_machine.rb
+++ soda_machine.rb
@@ -6,13 +6,21 @@ class SodaMachine
     @cash = args[:cash]
   end
 
+  # Soda machine will be able to return the number of sodas it contains.
   def current_inventory_count
+
   end
 
+  # Soda machine will be able to find a single soda with a given brand.
   def find_soda(soda_brand)
+
   end
 
+  # Soda machine will be able to sell a soda with a given brand. 
+  # When a soda is sold, it should be removed from the soda machine 
+  # and the price of the soda should be added to the soda machine's cash
   def sell(soda_brand)
+
   end
 
 end
