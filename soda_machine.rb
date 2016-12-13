1bda435f10771f113a30ff42eafa694f2ef96fa9
Lisa Nguyen <email": null>
2016-12-13 10:05:23 -0800

    saving CP2

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,18 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.find { |soda| soda.brand == soda_brand }
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand)
+      @cash += find_soda(soda_brand).price
+      @sodas.delete(find_soda(soda_brand))
+    end
   end
 
 end
