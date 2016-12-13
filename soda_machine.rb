f63306ef223d05a84e58cc28e85dade4cfaf4bd0
Tony <saric.tony@gmail.com>
2016-12-13 10:05:56 -0800

    adds code for soda machine

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,5 +1,11 @@
+require_relative "soda"
+
 class SodaMachine
   attr_reader :sodas, :cash
+
+  class Soda < SodaMachine
+  end
+
 
   def initialize(args = {})
     @sodas = args[:sodas]
@@ -7,12 +13,28 @@ class SodaMachine
   end
 
   def current_inventory_count
+  	@soda.count
   end
 
   def find_soda(soda_brand)
+  	if soda_brand == @soda[brand]
+  		soda_brand
+  	else
+  		nil
+  	end
   end
 
   def sell(soda_brand)
+  	total = 0
+  	@cash.each do |item|
+  		item.each do |price|
+  		total += item.price
+  		end
+  	end
   end
 
 end
+
+pepsi = SodaMachine.new
+p pepsi
+
