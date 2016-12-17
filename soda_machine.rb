46eafd7438eab72b535fb369358312a2fcc084ae
George B <ggbabayan@gmail.com>
2016-12-16 22:41:36 -0800

    refactor

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,26 @@
 class SodaMachine
   attr_reader :sodas, :cash
-
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
   end
 
   def current_inventory_count
+    @sodas.size
   end
 
   def find_soda(soda_brand)
+    @sodas.find {|soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand)
+      soda=find_soda(soda_brand)
+      @cash+=soda.price
+      sold_soda=soda
+      @sodas.delete(soda)
+    end
+    sold_soda
   end
 
 end
