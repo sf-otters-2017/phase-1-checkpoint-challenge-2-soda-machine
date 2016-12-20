65a5aad815d35907f836da9da26c85286227b345
Lisa Nguyen <lisathuanguyen@gmail.com>
2016-12-20 11:52:58 -0800

    refactor sell method

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,24 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
+    #counts the # of elements in the @sodas array
   end
 
   def find_soda(soda_brand)
+    @sodas.find {|soda| soda_brand == soda.brand}
+    #iterating over the @sodas array. going through each soda
+    #until the soda_brand finds a match with soda.brand
   end
 
   def sell(soda_brand)
+    soda = find_soda(soda_brand)
+    if find_soda(soda_brand)
+      @cash += soda.price
+      #add equals the price on the result of the find_soda result
+      @sodas.delete(soda)
+      #deletes the match from find_soda from the @sodas array
+    end
   end
 
 end
